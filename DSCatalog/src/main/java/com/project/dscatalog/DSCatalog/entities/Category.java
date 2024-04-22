452f52a00d7eb8c6e58c2bbe8c06dd5e0603f885
YgorGoesSoares <108436676+YgorGoesSoares@users.noreply.github.com>
2024-04-22 07:54:44 -0300

    CategoryResource

---
+++ DSCatalog/src/main/java/com/project/dscatalog/DSCatalog/entities/Category.java
@@ -0,0 +1,40 @@
+package com.project.dscatalog.DSCatalog.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+public class Category implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category category)) return false;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
