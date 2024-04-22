be1586e810bf5f9f89d06aa436b1d5ca9372e903
YgorGoesSoares <108436676+YgorGoesSoares@users.noreply.github.com>
2024-04-22 09:51:45 -0300

    Exception Handler

--- DSCatalog/src/main/java/com/project/dscatalog/DSCatalog/services/CategoryService.java
+++ DSCatalog/src/main/java/com/project/dscatalog/DSCatalog/services/CategoryService.java
@@ -3,12 +3,15 @@ package com.project.dscatalog.DSCatalog.services;
 import com.project.dscatalog.DSCatalog.dto.CategoryDTO;
 import com.project.dscatalog.DSCatalog.entities.Category;
 import com.project.dscatalog.DSCatalog.repositories.CategoryRepository;
+import com.project.dscatalog.DSCatalog.services.exceptions.EntityNotFoundException;
+import jakarta.persistence.EntityExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -23,4 +26,12 @@ public class CategoryService {
         return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 
     }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found."));
+        return new CategoryDTO(entity);
+    }
+
 }
